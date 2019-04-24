cf269ef026586d4646731983914c26fa77d73ac4
cooix <zhangyanmingjiayou@163.com>
2019-04-24 11:46:53 +0800

    docs(sql ) add sql notes

--- database-test.sql
+++ database-test.sql
@@ -1,6 +1,20 @@
-# 测试sql的数据库创建
-# 创建数据库 编码为utf8mb4;
-create database IF NOT EXISTS test default character set utf8mb4 collate utf8mb4_unicode_ci;
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : 本机
+Source Server Version : 50721
+Source Host           : 127.0.0.1:3306
+Source Database       : test
+
+Target Server Type    : MYSQL
+Target Server Version : 50721
+File Encoding         : 65001
+
+Date: 2019-04-24 11:45:40
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
 -- ----------------------------
 -- Table structure for course
 -- ----------------------------
@@ -9,16 +23,23 @@ CREATE TABLE `course` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `course_id` int(11) NOT NULL COMMENT '课程id',
   `course_name` varchar(255) DEFAULT NULL COMMENT '课程名称',
+  `tea_id` int(11) DEFAULT NULL COMMENT '授课教师ID',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COMMENT='课程信息表';
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COMMENT='课程信息表';
 
 -- ----------------------------
 -- Records of course
 -- ----------------------------
-INSERT INTO `course` VALUES ('1', '101', '语文');
-INSERT INTO `course` VALUES ('2', '102', '数学');
-INSERT INTO `course` VALUES ('3', '103', '历史');
-INSERT INTO `course` VALUES ('4', '104', '计算机');
+INSERT INTO `course` VALUES ('1', '101', '语文', '2019001');
+INSERT INTO `course` VALUES ('2', '102', '数学', '2019003');
+INSERT INTO `course` VALUES ('3', '103', '历史', '2019004');
+INSERT INTO `course` VALUES ('4', '104', '计算机', '2019002');
+INSERT INTO `course` VALUES ('5', '105', '教育技术学', '2019004');
+INSERT INTO `course` VALUES ('6', '106', '展览学', '2019002');
+INSERT INTO `course` VALUES ('7', '107', 'C语言编程', '2019002');
+INSERT INTO `course` VALUES ('8', '108', 'Java语言编程', '2019002');
+INSERT INTO `course` VALUES ('9', '109', '高等数学', '2019003');
+INSERT INTO `course` VALUES ('10', '110', '毛概', '2019004');
 
 -- ----------------------------
 -- Table structure for student
@@ -28,17 +49,25 @@ CREATE TABLE `student` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `stu_id` int(11) DEFAULT NULL COMMENT '学生学号',
   `stu_name` varchar(50) DEFAULT NULL COMMENT '学生姓名',
+  `stu_age` int(11) DEFAULT NULL COMMENT '学生年龄',
   `stu_sex` varchar(5) DEFAULT NULL COMMENT '性别',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COMMENT='学生信息表';
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COMMENT='学生信息表';
 
 -- ----------------------------
 -- Records of student
 -- ----------------------------
-INSERT INTO `student` VALUES ('1', '1001', '张三', '男');
-INSERT INTO `student` VALUES ('2', '1002', '李四', '女');
-INSERT INTO `student` VALUES ('3', '1003', '王五', '女');
-INSERT INTO `student` VALUES ('4', '1004', '刘柳', null);
+INSERT INTO `student` VALUES ('1', '1001', '张三', '14', '男');
+INSERT INTO `student` VALUES ('2', '1002', '李四', '15', '女');
+INSERT INTO `student` VALUES ('3', '1003', '王五', '15', '女');
+INSERT INTO `student` VALUES ('4', '1004', '刘柳', '14', '女');
+INSERT INTO `student` VALUES ('5', '1005', '李看书', '13', '男');
+INSERT INTO `student` VALUES ('6', '1006', '于强', '14', '男');
+INSERT INTO `student` VALUES ('7', '1007', '苏大强', '15', '女');
+INSERT INTO `student` VALUES ('8', '1008', '苏明宇', '12', '男');
+INSERT INTO `student` VALUES ('9', '1009', '苏有朋', '13', '女');
+INSERT INTO `student` VALUES ('10', '1010', '王丽丽', '13', '男');
+INSERT INTO `student` VALUES ('11', '1011', '王不输', '14', '女');
 
 -- ----------------------------
 -- Table structure for stu_grade
@@ -63,13 +92,13 @@ INSERT INTO `stu_grade` VALUES ('5', '1002', '102', '67');
 INSERT INTO `stu_grade` VALUES ('6', '1002', '103', '87');
 INSERT INTO `stu_grade` VALUES ('7', '1002', '104', '67');
 INSERT INTO `stu_grade` VALUES ('8', '1003', '101', '87');
-INSERT INTO `stu_grade` VALUES ('9', '1003', '102', '98');
+INSERT INTO `stu_grade` VALUES ('9', '1003', '105', '98');
 INSERT INTO `stu_grade` VALUES ('10', '1003', '103', '57');
 INSERT INTO `stu_grade` VALUES ('11', '1003', '104', '67');
 INSERT INTO `stu_grade` VALUES ('12', '1004', '101', '84');
-INSERT INTO `stu_grade` VALUES ('13', '1004', '102', '23');
+INSERT INTO `stu_grade` VALUES ('13', '1004', '105', '23');
 INSERT INTO `stu_grade` VALUES ('14', '1004', '103', '45');
-INSERT INTO `stu_grade` VALUES ('15', '1004', '104', '100');
+INSERT INTO `stu_grade` VALUES ('15', '1004', '104', '12');
 
 -- ----------------------------
 -- Table structure for teacher
@@ -81,12 +110,15 @@ CREATE TABLE `teacher` (
   `tea_name` varchar(50) DEFAULT NULL COMMENT '教师姓名',
   `tea_sex` varchar(5) DEFAULT NULL COMMENT '性别',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COMMENT='教师信息表';
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COMMENT='教师信息表';
 
 -- ----------------------------
 -- Records of teacher
 -- ----------------------------
 INSERT INTO `teacher` VALUES ('1', '2019001', '老生常', '男');
 INSERT INTO `teacher` VALUES ('2', '2019002', '王树忱', '女');
-INSERT INTO `teacher` VALUES ('3', '2019003', '六三三', '女');
+INSERT INTO `teacher` VALUES ('3', '2019003', '叶平', '女');
 INSERT INTO `teacher` VALUES ('4', '2019004', '刘柳二', '');
+INSERT INTO `teacher` VALUES ('5', '2019005', '李乐乐', '男');
+INSERT INTO `teacher` VALUES ('6', '2019006', '乔李斯', '女');
+INSERT INTO `teacher` VALUES ('7', '2019007', '李琉璃', '男');
--- question.md
+++ question.md
@@ -1,2 +1,72 @@
 # 数据库面试相关的sql问题记录
 
+
+- 表信息
+
+    [表信息sql](https://github.com/zhangymPerson/DBMS-note/blob/master/table-info.md)
+
+   [数据](https://github.com/zhangymPerson/DBMS-note/blob/master/database-test.sql) 
+
+- sql 练习
+```sql
+-- 查询“101”课程比“102”课程成绩高的所有学生的学号；
+select * from stu_grade;
+select a.stu_id from (select stu_id,grade from stu_grade where course_id = 101) a , (select stu_id,grade from stu_grade where course_id = 102) b where a.stu_id = b.stu_id AND a.grade > b.grade;
+
+
+-- 查询平均成绩大于60分的同学的学号和平均成绩；
+select * from stu_grade;
+select stu_id,AVG(grade) from stu_grade GROUP BY stu_id HAVING avg(grade) > 60;
+
+-- 查询所有同学的学号、姓名、选课数、总成绩；
+select a.stu_id, b.stu_name,COUNT(a.course_id),sum(a.grade) from stu_grade a  LEFT JOIN student b on a.stu_id = b.stu_id GROUP BY a.stu_id ; 
+
+
+-- 查询姓“李”的老师的个数；
+select * from teacher;
+select count(DISTINCT tea_id) from teacher t where t.tea_name like "李%";
+
+-- 查询没学过“叶平”老师课的同学的学号、姓名； not in 
+
+SELECT
+	*
+FROM
+	student
+WHERE
+	stu_id NOT IN (
+		SELECT DISTINCT
+			d.stu_id
+		FROM
+			stu_grade d
+		LEFT JOIN (
+			SELECT
+				b.course_id
+			FROM
+				(
+					SELECT
+						t.tea_id
+					FROM
+						teacher t
+					WHERE
+						t.tea_name = "叶平"
+				) a
+			LEFT JOIN course b ON a.tea_id = b.tea_id
+		) c ON c.course_id = d.course_id
+	);
+
+
+-- 查询学过“001”并且也学过编号“002”课程的同学的学号、姓名；
+-- inner join 是同时符合前后条件的查询
+select a.stu_id from stu_grade a INNER JOIN stu_grade b on a.stu_id = b.stu_id where a.course_id =101 and b.course_id = 102
+
+select a.stu_id,b.stu_name from (select a.stu_id from stu_grade a INNER JOIN stu_grade b on a.stu_id = b.stu_id where a.course_id =101 and b.course_id = 102) a LEFT JOIN student b on a.stu_id = b.stu_id;
+
+
+
+
+-- 查询课程编号“103”的成绩比课程编号“104”课程低的所有同学的学号、姓名；
+select a.stu_id FROM (select * from stu_grade where course_id = '103' ) a left JOIN (select * from stu_grade where course_id = '104')b on a.stu_id = b.stu_id where a.grade < b.grade;
+
+
+
+```
